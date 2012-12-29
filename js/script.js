a1043fbb136a787bc2950048a1c24accf08a9f7f
Bouke Regnerus <bouke@regner.us>
2012-12-29 22:41:20 +0100

    Small fixes, added view.js

--- js/script.js
+++ js/script.js
@@ -39,14 +39,14 @@ $(document).ready(function() {
 	}
 	
 	if ($("code").length > 0) {
-		head.js({highlight: "http://cdn.regner.us/js/highlight.js"});	
+		head.js({highlight: "//cdn.regner.us/js/highlight.js"});	
 		head.ready("highlight", function() {
 			$("head").append("<link>");
 			css = $("head").children(":last");
 			css.attr({
 				rel:  "stylesheet",
 				type: "text/css",
-				href: "http://cdn.regner.us/css/highlight/github.css"
+				href: "//cdn.regner.us/css/highlight/github.css"
 			});  
 			hljs.initHighlightingOnLoad();
 		});	
@@ -58,7 +58,7 @@ $(document).ready(function() {
 		css.attr({
 			rel:  "stylesheet",
 			type: "text/css",
-			href: "http://cdn.regner.us/css/animate.css"
+			href: "//cdn.regner.us/css/animate.css"
 		});
 	}
 	
@@ -78,4 +78,10 @@ $(document).ready(function() {
 	if($("html").attr("lang")=="en"){jQuery.timeago.settings.strings={prefixAgo:null,prefixFromNow:null,suffixAgo:"ago",suffixFromNow:"from now",seconds:"less than a minute",minute:"about a minute",minutes:"%d minutes",hour:"about an hour",hours:"about %d hours",day:"a day",days:"%d days",month:"about a month",months:"%d months",year:"about a year",years:"%d years",wordSeparator:" ",numbers:[]}}else if($("html").attr("lang")=="nl"){jQuery.timeago.settings.strings={prefixAgo:null,prefixFromNow:"",suffixAgo:"geleden",suffixFromNow:"van nu",seconds:"minder dan een minuut",minute:"ongeveer een minuut",minutes:"%d minuten",hour:"ongeveer een uur",hours:"ongeveer %d uur",day:"een dag",days:"%d dagen",month:"ongeveer een maand",months:"%d maanden",year:"ongeveer een jaar",years:"%d jaar",wordSeparator:" ",numbers:[]}}else if($("html").attr("lang")=="de"){jQuery.timeago.settings.strings={prefixAgo:"vor",prefixFromNow:"in",suffixAgo:"",suffixFromNow:"",seconds:"wenigen Sekunden",minute:"etwa einer Minute",minutes:"%d Minuten",hour:"etwa einer Stunde",hours:"%d Stunden",day:"etwa einem Tag",days:"%d Tagen",month:"etwa einem Monat",months:"%d Monaten",year:"etwa einem Jahr",years:"%d Jahren"}}else if($("html").attr("lang")=="fr"){jQuery.timeago.settings.strings={prefixAgo:"il y a",prefixFromNow:"d'ici",seconds:"moins d'une minute",minute:"environ une minute",minutes:"environ %d minutes",hour:"environ une heure",hours:"environ %d heures",day:"environ un jour",days:"environ %d jours",month:"environ un mois",months:"environ %d mois",year:"un an",years:"%d ans"}}
 	
 	$("time.timeago").timeago();
+	
+	head.ready("view", function() {
+		new View($(".view"));
+	});
+	
+	head.js({view: "//cdn.regner.us/js/view.js"});
 });
