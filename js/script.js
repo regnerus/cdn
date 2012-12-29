1a2a7deee830c5af79c9472f1a27a38b75842602
Bouke Regnerus <bouke@regner.us>
2012-12-29 16:47:14 +0100

    Added javascript timago.js

--- js/script.js
+++ js/script.js
@@ -62,4 +62,26 @@ $(document).ready(function() {
 	$('textarea').autogrow();
 	
 	$('a').smoothScroll();
+	
+	jQuery.timeago.settings.strings = {
+	  prefixAgo: null,
+	  prefixFromNow: "",
+	  suffixAgo: "geleden",
+	  suffixFromNow: "van nu",
+	  seconds: "minder dan een minuut",
+	  minute: "ongeveer een minuut",
+	  minutes: "%d minuten",
+	  hour: "ongeveer een uur",
+	  hours: "ongeveer %d uur",
+	  day: "een dag",
+	  days: "%d dagen",
+	  month: "ongeveer een maand",
+	  months: "%d maanden",
+	  year: "ongeveer een jaar",
+	  years: "%d jaar",
+	  wordSeparator: " ",
+	  numbers: []
+	};
+	
+	$("time.timeago").timeago();
 });
